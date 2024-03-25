f0f812ac822322d14d9674e786b9307c261f4b7e
mikeC2493 <164425119+mikeC2493@users.noreply.github.com>
2024-03-25 02:04:19 -0400

    Updated Website Design site, updated Story and Menu

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,15 +1,15 @@
 
 window.onload = function(){
-    var proteinTables = ["steakType","fishType","chickenType"];
+    var proteinTables = ["steakType","fishType","chickenType","pastaType","potatoType","vegetableType", 
+"pieType", "cakeType", "icecreamType"];
     for(var i=0;i<proteinTables.length;i++){
         document.getElementById(proteinTables[i]).style.display = "none";
     }
 }
 
-function displayTable(imageID){
+function displayProteinTable(imageID){
     var proteinArray = ["cowImage","fishImage","chickenImage"];
     var selectedImage = imageID;
-    console.log(selectedImage);
     for(var i=0;i<proteinArray.length;i++){
         if(proteinArray[i] == selectedImage){
             document.getElementById(proteinArray[i]).classList.add("selectedImage");
@@ -18,10 +18,10 @@ function displayTable(imageID){
             document.getElementById(proteinArray[i]).classList.remove("selectedImage");
         }
     }
-displayType(selectedImage);
+displayProteinType(selectedImage);
 }
 
-function displayType(foodType){
+function displayProteinType(foodType){
     var typeMenu = foodType;
     if(typeMenu == "cowImage"){
         document.getElementById("steakType").style.display = "block";
@@ -41,3 +41,70 @@ function displayType(foodType){
     
 }
 
+function displaySidesTable(imageID){
+    var sidesArray = ["pastaImage","potatoImage","vegetableImage"];
+    var selectedImage = imageID;
+    for(var i=0;i<sidesArray.length;i++){
+        if(sidesArray[i] == selectedImage){
+            document.getElementById(sidesArray[i]).classList.add("selectedImage");
+        }
+        else{
+            document.getElementById(sidesArray[i]).classList.remove("selectedImage");
+        }
+    }
+displaySidesType(selectedImage);
+}
+
+function displaySidesType(foodType){
+    var typeMenu = foodType;
+    if(typeMenu == "pastaImage"){
+        document.getElementById("pastaType").style.display = "block";
+        document.getElementById("potatoType").style.display = "none";
+        document.getElementById("vegetableType").style.display = "none";
+    }
+    else if(typeMenu == "potatoImage"){
+        document.getElementById("potatoType").style.display = "block";
+        document.getElementById("pastaType").style.display = "none";
+        document.getElementById("vegetableType").style.display = "none";
+    }
+    else if(typeMenu == "vegetableImage"){
+        document.getElementById("vegetableType").style.display = "block";
+        document.getElementById("pastaType").style.display = "none";
+        document.getElementById("potatoType").style.display = "none";
+    }
+    
+}
+
+function displayDessertTable(imageID){
+    var dessertArray = ["pieImage","cakeImage","icecreamImage"];
+    var selectedImage = imageID;
+    for(var i=0;i<dessertArray.length;i++){
+        if(dessertArray[i] == selectedImage){
+            document.getElementById(dessertArray[i]).classList.add("selectedImage");
+        }
+        else{
+            document.getElementById(dessertArray[i]).classList.remove("selectedImage");
+        }
+    }
+displayDessertType(selectedImage);
+}
+
+function displayDessertType(foodType){
+    var typeMenu = foodType;
+    if(typeMenu == "pieImage"){
+        document.getElementById("pieType").style.display = "block";
+        document.getElementById("cakeType").style.display = "none";
+        document.getElementById("icecreamType").style.display = "none";
+    }
+    else if(typeMenu == "cakeImage"){
+        document.getElementById("cakeType").style.display = "block";
+        document.getElementById("pieType").style.display = "none";
+        document.getElementById("icecreamType").style.display = "none";
+    }
+    else if(typeMenu == "icecreamImage"){
+        document.getElementById("icecreamType").style.display = "block";
+        document.getElementById("pieType").style.display = "none";
+        document.getElementById("cakeType").style.display = "none";
+    }
+    
+}
